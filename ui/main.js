9be1bb7ee1b1eecbc668c7698d7576244d675b0b
Mitanshu1997 <khurana.mitanshu@gmail.com>
2018-02-27 23:22:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,6 +9,14 @@ console.log('loaded rest....heehaa!');
 
 //Move the image
 var img = document.getElementById('madi');
+
+var marginleft = 0;
+
+function moveRight(){
+    marginleft = marginleft + 10;
+    img.style.marginleft = marginleft + "px";
+}
 img.onclick = function(){
+    var interval = setInterval(moveRight, 100);
     img.style.marginleft = "100px";
 };
