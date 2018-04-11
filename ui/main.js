658df2544fdf50358eafb3f55d45982b76ec1ae1
Mitanshu1997 <khurana.mitanshu@gmail.com>
2018-04-11 12:11:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,8 +47,7 @@ button.onclick = function() {
 };
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     //Make a request to the server and send the name
@@ -75,6 +74,8 @@ submit.onclick = function() {
     };
     
     //Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://khuranamitanshu.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
     
