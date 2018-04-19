342d6c684dd7cf0e4921047c4cf8a9fe4180a8e6
Mitanshu1997 <khurana.mitanshu@gmail.com>
2018-04-19 13:14:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/test-db', function(req, res) {
        if(err) {
            res.status(500).send(err.toString());
        } else {
-           res.send(JSON.stringify(result));
+           res.send(JSON.stringify(result.rows));
        }
     });
 });
@@ -119,11 +119,22 @@ app.get('/submit-name/', function(req, res) { //Using a query parameter...../sub
 });
 
 
-app.get('/:articleName', function(req, res) {
+app.get('/articles/:articleName', function(req, res) {
   // articleName == article-one
   //articles[articleName] = {} content object for article-one
-  var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+  
+  pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function(err, result) {
+   if(err){
+       res.status(500).send(err.toString());
+   } else {
+       if (result.rows.length === 0) {
+           res.status(404).send("Article not found");
+       } else {
+           var articleData = result.rows[0];
+           res.send(createTemplate(articleData));
+       }
+   }
+  });
 });
 
 app.get('/ui/style.css', function (req, res) {
