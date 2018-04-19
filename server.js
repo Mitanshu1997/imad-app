e6ad24c4030bd3bf9115fe3d9a0562189843f95f
Mitanshu1997 <khurana.mitanshu@gmail.com>
2018-04-19 13:01:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/', function (req, res) {
 });
 
 var pool = new Pool(config);
-app.get('test-db', function(req, res) {
+app.get('/test-db', function(req, res) {
     //make a select request
     //return a reponse with the results
     pool.query('SELECT * FROM test', function(err, result) {
